f028777096734a2b402f2d9dda5dd667bc3a260d
sha <1750875477@qq.com>
2023-11-30 15:15:26 +0800

    Updates

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import '@/styles/index.scss'
-import router from '@/routes';
-import App from './App.vue'
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
